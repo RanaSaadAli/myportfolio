626c97237d75d67a1ce605ff2bcbfb281c9a57fb
RanaSaadAli <saadalikhanrana86@gmail.com>
2026-06-18 11:29:18 +0500

    add prometheus metrics instruments

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from routes.public import public
 from routes.admin import admin
-
+from prometheus_fastapi_instrumentator import Instrumentator
 
 app = FastAPI()
+Instrumentator().instrument(app).expose(app)
 app.mount("/static", StaticFiles(directory="static"), name="static")
 app.include_router(public)
 app.include_router(admin)
